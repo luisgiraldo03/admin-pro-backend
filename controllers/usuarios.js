f99b722a00bc2bdfe8562dddd2768a8e2fbda4ea
Luis Giraldo <legiraldoj@eafit.edu.co>
2021-05-17 19:13:25 -0500

    token

---
+++ controllers/usuarios.js
@@ -0,0 +1,147 @@
+const { response } = require('express');
+const bcrypt = require('bcryptjs');
+const Usuario = require('../models/usuario');
+const { generarJWT } = require('../helpers/jwt');
+
+const getUsuarios = async (req, res) => {
+
+    const usuario = await Usuario.find({}, 'nombre email role google');
+
+    res.json({
+        ok: true,
+        usuario,
+        uid: req.uid
+    });
+}
+
+const crearUsuario = async (req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        const existeEmail = await Usuario.findOne({ email });
+
+        if (existeEmail) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya está registrado'
+            });
+        }
+
+        const usuario = new Usuario(req.body);
+
+        //encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
+
+        //guardar usuario
+        await usuario.save();
+
+        // Generar el TOKEN - JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            ok: true,
+            usuario,
+            token
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado... revisar logs'
+        });
+    }
+}
+
+const actualizarUsuario = async (req, res = response) => {
+
+    // TODO: Validar token y comprobar si es el usuario correcto
+
+    const uid = req.params.id;
+
+
+    try {
+
+        const usuarioDB = await Usuario.findById(uid);
+
+        if (!usuarioDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe un usuario por ese id'
+            });
+        }
+
+        // Actualizaciones
+        const { password, google, email, ...campos } = req.body;
+
+        if (usuarioDB.email !== email) {
+
+            const existeEmail = await Usuario.findOne({ email });
+            if (existeEmail) {
+                return res.status(400).json({
+                    ok: false,
+                    msg: 'Ya existe un usuario con ese email'
+                });
+            }
+        }
+
+        campos.email = email;
+        const usuarioActualizado = await Usuario.findByIdAndUpdate(uid, campos, { new: true });
+
+        res.json({
+            ok: true,
+            usuario: usuarioActualizado
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado'
+        })
+    }
+}
+
+const borrarUsuario = async (req, res = response) => {
+
+    const uid = req.params.id;
+
+    try {
+
+        const usuarioDB = await Usuario.findById(uid);
+
+        if (!usuarioDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe un usuario por ese id'
+            });
+        }
+
+        await Usuario.findByIdAndDelete(uid);
+
+        res.json({
+            ok: false,
+            msg: 'Usuario eliminado'
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado'
+        });
+    }
+
+}
+
+module.exports = {
+    getUsuarios,
+    crearUsuario,
+    actualizarUsuario,
+    borrarUsuario
+}
--- index.js
+++ index.js
@@ -7,18 +7,18 @@ require('dotenv').config();
 const app = express();
 
 //cors
-app.use(cors);
+app.use(cors());
+
+//lectura y parseo del body
+app.use(express.json());
 
 //Base de datos
 dbConnection();
 
+//rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/login', require('./routes/auth'));
 
-app.get('/', (req, res) => {
-    res.json({
-        ok: true,
-        msg: 'Hola Mundo'
-    });
-});
 
 app.listen(process.env.PORT, () => {
     console.log("listening port: " + process.env.PORT);
